4ec8275de7ef02aeb269b163a0788de9b339eedc
Anton Yarkov <ayarkov@accesssoftek.com>
2016-11-23 14:31:46 +0300

    Blogs.

---
+++ 4 - Blogs.sql
@@ -0,0 +1,74 @@
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#smartclothes', 'Опубликован официальный пост-релиз «Российской недели текстильной и легкой промышленности»', 'fashion', '2016-03-04 03:04:07.000', 'http://riamoda.ru/news/news-opublikovan-ofitsialnyj-post-reliz-rossi.html', '2', 'riamoda.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#smartclothes', 'Nike представил кроссовки с технологиеи? автоматическои? шнуровки', 'fashion', '2016-03-21 00:41:47.000', 'https://fashionunited.ru/novostee/moda/nike-predstavil-krossovki-s-tekhnologiei-avtomaticheskoi-shnurovki/2016032114780', '2', 'fashionunited.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Tissot Smart-Touch — первые по-настоящему «умные» часы от Swatch', 'tech, новости hardware', '2016-03-21 14:01:59.000', 'http://www.3dnews.ru/930205/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Tag Heuer наметила выпуск коллекции смарт-часов на 2017 год', 'tech, новости hardware', '2016-03-20 06:24:38.000', 'http://www.3dnews.ru/930136/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Apple Watch займут половину рынка «умных» часов в 2016 году', 'tech, новости hardware', '2016-03-20 02:35:30.000', 'http://www.3dnews.ru/930122/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Майкл Корс представил свои первые смарт-часы Michael Kors Access', 'tech, новости hardware', '2016-03-20 02:13:20.000', 'http://www.3dnews.ru/930109/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Xiaomi готовит к релизу фирменный шоссейный велосипед за $3000', 'tech, новости hardware', '2016-03-18 12:39:39.000', 'http://www.3dnews.ru/930073/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Смарт-часы Samsung Gear S2 by de Grisogono получили более 100 бриллиантов', 'tech, новости hardware', '2016-03-18 11:02:08.000', 'http://www.3dnews.ru/930071/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Обзор наушников Bang&Olufsen H2', 'tech', '2016-01-19 16:34:13.000', 'http://www.mobile-review.com/mp3/review/bang-n-olufsen-h2.shtml', '2', 'mobile-review-reviews', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Will "Night Modes" on My Smartphone or Tablet Actually Help Me Get Better Sleep?', 'tech, sleep, smartphones, apple, android, ios, night shift, night mode, science, studies, editors picks', '2016-03-22 04:00:00.000', 'http://feeds.gawker.com/~r/lifehacker/vip/~3/MDRNhqmoi_8/will-night-modes-on-my-smartphone-or-tablet-actually-1766261703', '1', 'Lifehacker', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Samsung wants you to stop checking your phone while on your motorbike', 'tech, gadgets', '2016-03-22 04:17:34.000', 'http://feedproxy.google.com/~r/TheNextWeb/~3/rEtYeuhI5lk/', '1', 'The Next Web', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#startups', 'This magic t-shirt provides you mind blowing Anatomy Adventure. Inspiring and innovative', 'tech, startups, kickstarter, health, virtual reality, vr', '2016-03-05 09:02:45.000', 'http://www.curiscope.com/', '1', 'GIGadgets', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#startups', 'Particle - a platform for developing an Internet of Things product.', 'tech, startups, platform, mobility, 3g, wifi, iot, internet of things, arduino, raspberry pi', '2016-03-22 09:02:45.000', 'https://www.particle.io', '1', 'Particle', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Компания Bvlgari объявила о сотрудничестве с Mastercard в рамках проекта Diagono E-Magnesium', 'fashion', '2016-03-22 08:11:48.000', 'http://riamoda.ru/news/news-kompanija-bvlgari-objavila-o-sotrudniche.html', '2', 'riamoda.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Today''s Best Deals: Nest Thermostat, Ditto, SSD Enclosure, and More', 'tech, kinja deals, deals, flood, daily deals, dealhacker, dealzmodo, moneysaver', '2016-03-22 07:07:45.000', 'http://feeds.gawker.com/~r/lifehacker/vip/~3/XWmjb60kzTQ/todays-best-deals-nest-thermostat-ditto-ssd-enclosur-1766356674', '1', 'Lifehacker', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Why the Apple Watch price cut is smart thinking', 'tech, gadgets, mobile, tc, wearables, apple watch, steve wozniak, apple, wearables, fashion', '2016-03-22 09:49:28.000', 'http://feedproxy.google.com/~r/Techcrunch/~3/qQOA8MX6Tc8/', '1', 'Techcrunch', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#startups', 'Like A Glove - smart leggins with smartphone application to find brands, styles and sizes that fit you. ', 'smart clothes, mobile, wearables', '2016-03-22 09:49:28.000', 'http://www.likeaglove.me/', '1', 'LikeAGlove', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#startups', 'Like A Glove - умные леггинсы с мобильным приложением, которые позволяют подбирать одежду прямо под ваши размеры.', 'smart clothes, mobile, wearables', '2016-03-22 09:49:28.000', 'http://www.likeaglove.me/', '2', 'LikeAGlove', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#startups', 'Магическая майка позволяет увидеть внутренние органы одевшего её человека', 'tech, startups, kickstarter, health, virtual reality, vr', '2016-03-22 09:49:28.000', 'http://www.curiscope.com/', '2', 'GIGadgets', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#startups', 'Particle - платформа разработки устройств для интернета вещей.', 'tech, startups, platform, mobility, 3g, wifi, iot, internet of things, arduino, raspberry pi', '2016-03-22 09:02:45.000', 'https://www.particle.io', '2', 'Particle', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Samsung будет бесплатно менять ремешки к Gear S2 при повреждении', 'tech, новости hardware', '2016-03-24 01:53:31.000', 'http://www.3dnews.ru/930330/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Tesla использует Google Glass на сборке автомобилей', 'tech, новости hardware', '2016-03-23 06:51:32.000', 'http://www.3dnews.ru/930350/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Китайский интернет-гигант Alibaba представит смарт-автомобиль', 'tech, новости hardware', '2016-03-23 03:58:07.000', 'http://www.3dnews.ru/930369/?feed', '2', '3dnews.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#smartclothes', 'Best smart clothes: Wearables to improve your life', 'fashion, tech, smart clothes', '2016-01-26 04:00:00.000', 'http://www.pocket-lint.com/news/131980-best-smart-clothes-wearables-to-improve-your-life', '1', 'PocketLint', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#smartclothes', 'The weird and wonderful from the Wearable Tech Show 2016', 'fashion, tech, smart clothes', '2016-01-26 04:00:00.000', 'http://www.stuff.tv/news/weird-and-wonderful-wearable-tech-show-2016', '1', 'Stuff.tv', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#video', 'Would you wear this one? :)', 'tech, robotics', '2016-03-22 04:00:00.000', 'NULL', '1', 'Spotwright', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#video', 'Вы бы одели такой? :)', 'tech, robotics', '2016-03-22 04:00:00.000', 'NULL', '2', 'Spotwright', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#smartclothes', 'Принты SS17 - подиумные коллекции женской одежды', 'fashion', '2016-10-26 01:34:15.000', 'https://fashionunited.ru/novostee/moda/printy-ss17-podiumnye-kollektsii-zhenskoj-odezhdy/2016102616484', '2', 'fashionunited.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#smartclothes', 'Как будут платить в Рунете в 2020 году?', 'tech, конференции, блог компании payonline, riw, russian interactive week, конференция, рунет, мероприятия, платежи, прогнозы, payonline', '2016-10-28 05:13:37.000', 'https://habrahabr.ru/post/312368/?utm_source=habrahabr&utm_medium=rss&utm_campaign=interesting', '2', 'habrahabr', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Jabra''s Sport Coach headphones count my reps so I don''t have to', 'tech, av, bluetooth, bluetoothheadphones, engadgetirl, fitness, gadgetry, gadgets, gear, headphones, irl, jabra, review, sportscoachspecialedition, uk-reviews, wearables', '2016-10-29 09:30:00.000', 'https://www.engadget.com/2016/10/29/jabra-sport-coach-headphones-review/', '1', 'Engadget', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#smartclothes', 'Zip around Taiwan on the faster Gogoro S electric scooter', 'tech, battery, bike, electricbike, electricscooter, electricvehicle, g1-s, gear, gogoro, gogoros, gostation, scooter, smartscooter, taiwan, transportation, vehicle, video', '2016-10-28 14:35:00.000', 'https://www.engadget.com/2016/10/28/gogoro-s-performance-electric-scooter/', '1', 'Engadget', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'Today''s Best Deals: Crock-Pot, All-Clad, Starship Enterprise, and More', 'tech, kinja deals, deals, daily deals, weekend', '2016-10-29 05:27:00.000', 'http://feeds.gawker.com/~r/lifehacker/vip/~3/Hf-oodErADs/todays-best-deals-crock-pot-all-clad-starship-enterp-1788359971', '1', 'Lifehacker', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#devices', 'How to Fake ''Before and After'' Fitness Photos', 'tech, evil week, health, fitness, psychology, weight loss, marketing, evil, editors picks, feature', '2016-10-28 08:00:00.000', 'http://feeds.gawker.com/~r/lifehacker/vip/~3/lCNxjAZ9TgE/how-to-fake-before-and-after-fitness-photos-1788276763', '1', 'Lifehacker', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#startups', 'Tty.Fit - new Russian startup will change world market of shoes', 'tech, startups, kickstarter, health', '2016-10-18 08:00:00.000', 'https://hightech.fm/2016/10/18/try-fit', '1', 'hightech.fm', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#startups', 'Технология российского стартапа изменит мировой рынок обуви6', 'tech, startups, kickstarter, health', '2016-10-18 08:00:00.000', 'https://hightech.fm/2016/10/18/try-fit', '2', 'hightech.fm', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#ai', 'Reima x Suunto: bluetooth-одежда для детей', 'fashion', '2016-11-02 01:05:04.000', 'https://fashionunited.ru/novostee/moda/reima-x-suunto-bluetooth-odezhda-dlya-detej/2016110216542', '2', 'fashionunited.ru', '2016-11-17 17:50:18.060')
+INSERT INTO [dbo].[Blog]([Theme],[Header],[Tags],[PublishDate],[SourceUrl],[LanguageId],[ProviderName],[Created]) VALUES (
+'#smartclothes', 'Шопинг В Instagram можно будет купить понравившиеся товары - РБК', 'tech, наука и техника', '2016-11-02 06:51:29.000', 'http://news.google.com/news/url?sa=t&fd=R&ct2=ru_ru&usg=AFQjCNH-9_xzhfSBIp600Jh6kaqxpjLpsw&clid=c3a7d30bb8a4878e06b80cf16b898331&cid=52779880544963&ei=TycaWLDMC8LPhAHctKmoAQ&url=http://style.rbc.ru/guide/shopping/5819e7839a79476063853e7c', '2', 'tech', '2016-11-17 17:50:18.060')
